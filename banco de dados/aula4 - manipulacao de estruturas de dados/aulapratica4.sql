32fbb60a7f9d36668c652a203907f80bdffac570
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-05-08 22:37:55 -0300

    update

--- banco de dados/aula4 - manipulacao de estruturas de dados/aulapratica4.sql
+++ banco de dados/aula4 - manipulacao de estruturas de dados/aulapratica4.sql
@@ -1,80 +1,64 @@
-create database aulapratica4;
+show databases;
 use aulapratica4;
 
-create table estado (
-    id int,
-    nome varchar(50),
-    sigla char(02),
-    primary key(id)
-);
+select * from estado;
+select * from cidade;
+select * from cliente;
+select * from funcionario;
+select * from pergunta;
 
-create table cidade (
-    id int,
-    nome varchar(100),
-    estadoId int,
-    primary key(id)
-);
+-- Atualizar dados
+update funcionario
+set cidadeID = (select id from cidade where nome = 'Recife')
+where matricula = 2;
+update funcionario
+set nome = 'Joao da Silva',
+departamento = 3
+where matricula = 2;
+select * from cidade;
+insert into cidade(id, nome, estadoID) values (7, 'Londrina', 1);
 
-create table cliente (
-    id int auto_increment,
-    nome varchar(100),
-    genero char(01),
-    nascimento date,
-    salario decimal(10,2),
-    email varchar(120) unique, 
-    cidadeId int not null,
-    constraint pkCliente primary key (id),
-    constraint fkClienteCidade foreign key (cidadeId) references cidade(id)
-    on delete no action on update no action
-);
+-- Deletar dados
+select * from cliente;
+delete from cliente where id = 5;
+delete from cliente where nome = 'Helena Magalhaes';
+delete from funcionario
+where departamento = 1
+and genero = 'M';
+delete from cliente where cidadeId in (select id from cidade where nome = 'Curitiba');
+delete from cliente;
+truncate table cliente;
 
-create table funcionario (
-    matricula int not null,
-    nome varchar(100),
-    genero char(01),
-    nascimento date,
-    salario decimal(10,2),
-    departamento int,
-    cargo int,
-    gerente int,
-    email varchar(120),
-    cidadeId int,
-    constraint pkFuncionario primary key (matricula)
-);
+-- Filtrar dados
+select * from cliente where genero = 'F';
+select * from funcionario
+where salario >= 5000
+and salario <= 8000;
+select * from cidade where nome is not null;
+-- like
+select nome from cliente where nome like '%Silva%';
+select nome from cliente where nome like '%Silva';
+select nome from cliente where nome like 'Silva%';
+-- in
+select nome, cidadeId from cliente where cidadeId in (1, 2, 4);
+select * from funcionario where cidadeId = 1 or cidadeId = 2 or cidadeId = 4;
+-- between
+select * from funcionario where cidadeId between 1 and 4;
+select * from funcionario where cidadeId not between 1 and 4;
+select * from funcionario where cidadeId >= 1 and cidadeId <= 4;
 
-create table pergunta (
-    id int,
-    pergunta varchar(100),
-    primary key(id)
-);
+select * from funcionario order by nome desc, salario asc; 
+select * from funcionario order by 3 asc;
 
-insert into estado values (1, 'Paraná', 'PR');
-insert into estado values (2, 'São Paulo', 'SP');
-insert into estado values (3, 'Pernambuco', 'PE');
-insert into estado values (4, 'Pará', 'PA');
-insert into estado values (5, 'Rio Grande do Sul', 'RS');
+-- Limit
+select * from funcionario LIMIT 3;
+select * from funcionario LIMIT 3, 2;
 
-insert into cidade values (1, 'Bagé', 5);
-insert into cidade values (2, 'Curitiba', 1);
-insert into cidade values (3, 'Recife', 3);
-insert into cidade values (4, 'São Paulo', null);
-insert into cidade values (5, 'Porto Alegre', null);
-insert into cidade values (6, 'Olinda', 3);
-
-insert into pergunta values (1, 'Qual a sua função?');
-insert into pergunta values (2, 'Avalie a sua gerência.');
-
-insert into cliente values (1, 'Helena Magalhães', 'F', '2000-01-01', 12500.99, 'helena@email.com', 2),
-                           (2, 'Nicolas', 'M', '2002-12-10', 8500, 'nicolas@email.com', 3),
-                           (3, 'Ana Rosa Silva', 'F', '1996-12-31', 8500, 'ana.rosa@email.com', 1),
-                           (4, 'Tales Heitor Souza', 'M', '2000-10-01', 7689, 'tales.heitor@email.com', 1),
-                           (5, 'Bia Meireles', 'F', '2002-03-14', 9450, 'bia.meireles@email.com', 2),
-                           (6, 'Pedro Filho', 'M', '1998-05-22', 6800, 'pedro.filho@email.com', 5),
-                           (7, 'Helena Magalhães', 'F', '1994-08-10', 8600, 'helena.magalhaes@email.com', 4);
-
-insert into funcionario values (1, 'Ana Rosa', 'F', '1996-12-31', 8500, 1, 1, null,'ana.rosa@email.com', 1),
-                               (2, 'Tales Heitor', 'M', '2000-10-01', 7689, 1, 2, 1, 'tales.heitor@email.com', null),
-                               (3, 'Bia Meireles', 'F', '2002-03-14', 9450, 1, 2, 1, 'bia.meireles@email.com', 2),
-                               (4, 'Pedro Filho', 'M', '1998-05-22', 6800, 3, 3, 2, 'pedro.filho@email.com', 4),
-                               (5, 'Helena Magalhães', 'F', '2000-01-01', 12500.99, 4, 5, 2, 'helena@email.com', 6),
-                               (6, 'Nicolas Pinto', 'M', '2002-12-10', 8500, 6, 3, null, 'nicolas.pinto@email.com', 5);
+-- Comando case
+select nome,
+    case
+        when genero = 'M' then 'Masculino'
+        when genero = 'F' then 'Feminino'
+        else 'Outros'
+    end as 'Gênero'
+from funcionario;
