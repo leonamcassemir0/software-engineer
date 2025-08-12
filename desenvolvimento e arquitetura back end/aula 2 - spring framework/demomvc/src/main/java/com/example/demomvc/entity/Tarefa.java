cae95c3d8ccf68821aaaa782c64c8fcabbd6a56d
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2025-08-12 19:48:24 -0300

    Aula prática da aula 3 da matéria de desenvolvimento back-end terminada

---
+++ desenvolvimento e arquitetura back end/aula 2 - spring framework/demomvc/src/main/java/com/example/demomvc/entity/Tarefa.java
@@ -0,0 +1,60 @@
+package com.example.demomvc.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Tarefa {
+	private Long id;
+	private String nome;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate dataEntrega;
+	private String responsavel;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	@Override
+	public String toString() {
+		return "Tarefa [id=" + id + ", nome=" + nome + ", dataEntrega=" + dataEntrega + ", responsavel=" + responsavel
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataEntrega, id, nome, responsavel);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(dataEntrega, other.dataEntrega) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(responsavel, other.responsavel);
+	}
+
+}
