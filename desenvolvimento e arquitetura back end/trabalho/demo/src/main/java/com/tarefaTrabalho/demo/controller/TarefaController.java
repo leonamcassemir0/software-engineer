faff7c95fa5eddb6731fb68a1b8561b7d481cd65
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2025-08-26 21:57:31 -0300

    Continuando o trabalho de JAVA

--- desenvolvimento e arquitetura back end/trabalho/demo/src/main/java/com/tarefaTrabalho/demo/controller/TarefaController.java
+++ desenvolvimento e arquitetura back end/trabalho/demo/src/main/java/com/tarefaTrabalho/demo/controller/TarefaController.java
@@ -1,7 +1,7 @@
-package com.example.demo.controller;
+package com.tarefaTrabalho.demo.controller;
 
-import com.example.demo.model.Tarefa;
-import com.example.demo.repository.TarefaRepository;
+import com.tarefaTrabalho.demo.model.Tarefa;
+import com.tarefaTrabalho.demo.repository.TarefaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +17,19 @@ public class TarefaController {
         this.repository = repository;
     }
 
+    // Criar
     @PostMapping
     public Tarefa criar(@RequestBody Tarefa tarefa) {
         return repository.save(tarefa);
     }
 
+    // Listar todas
     @GetMapping
     public List<Tarefa> listar() {
         return repository.findAll();
     }
 
+    // Buscar por ID
     @GetMapping("/{id}")
     public ResponseEntity<Tarefa> buscar(@PathVariable Long id) {
         return repository.findById(id)
@@ -34,18 +37,20 @@ public class TarefaController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    // Atualizar
     @PutMapping("/{id}")
     public ResponseEntity<Tarefa> atualizar(@PathVariable Long id, @RequestBody Tarefa tarefa) {
         return repository.findById(id)
                 .map(record -> {
-                    record.setNome(tarefa.getNome());
+                    record.setId(tarefa.getNome());
                     record.setDataEntrega(tarefa.getDataEntrega());
-                    record.setResponsavel(tarefa.getResponsavel());
+                    record.setId(tarefa.getResponsavel());
                     Tarefa atualizado = repository.save(record);
                     return ResponseEntity.ok().body(atualizado);
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    // Deletar
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletar(@PathVariable Long id) {
         return repository.findById(id)
--- desenvolvimento e arquitetura back end/trabalho/demo/src/main/java/com/tarefaTrabalho/demo/repository/TarefaRepository.java
+++ desenvolvimento e arquitetura back end/trabalho/demo/src/main/java/com/tarefaTrabalho/demo/repository/TarefaRepository.java
@@ -1,7 +1,7 @@
-package com.example.demo.repository;
+package com.tarefaTrabalho.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Tarefa;
+import com.tarefaTrabalho.demo.model.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
